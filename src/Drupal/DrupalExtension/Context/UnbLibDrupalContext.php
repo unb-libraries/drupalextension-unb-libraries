638049874c539e9036787e9bd070ccc9be0bddb8
jsanford <jsanford@unb.ca>
2019-10-30 15:03:21 -0300

    IN-196 Remove duplicate method from parent

--- src/Drupal/DrupalExtension/Context/UnbLibDrupalContext.php
+++ src/Drupal/DrupalExtension/Context/UnbLibDrupalContext.php
@@ -9,36 +9,6 @@ use Drupal\DrupalExtension\Context\DrupalContext;
  */
 class UnbLibDrupalContext extends DrupalContext {
 
-  /**
-   * Creates and authenticates a user with the given role(s).
-   *
-   * @Given I am logged in as a user with the :role role(s)
-   * @Given I am logged in as a/an :role
-   */
-    public function assertAuthenticatedByRole($role) {
-        // Check if a user with this role is already logged in.
-        if (!$this->loggedInWithRole($role)) {
-            // Create user (and project)
-            $user = (object) array(
-            'name' => $this->getRandom()->name(8),
-            'pass' => $this->getRandom()->name(16),
-            'role' => $role,
-            );
-            $user->mail = "{$user->name}@example.com";
-
-            $this->userCreate($user);
-
-            $roles = explode(',', $role);
-            $roles = array_map('trim', $roles);
-            foreach ($roles as $role) {
-                if (!in_array(strtolower($role), array('authenticated', 'authenticated user'))) {
-                    $this->getDriver()->userAddRole($user, $role);
-                }
-            }
-
-            // Login.
-            $this->login($user);
-        }
-    }
+  // Add public methods here.
 
 }
